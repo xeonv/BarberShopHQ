b4dfa8e9e18dfea4bc07b0af0da531358d465b73
Valery <xeonv@mail.ru>
2017-03-27 23:14:14 +0400

    Fix bug in app.rb and save clients to database

--- app.rb
+++ app.rb
@@ -12,8 +12,31 @@ end
 
 class Barber < ActiveRecord::Base
 end
+before do
+	@barbers = Barber.order "created_at DESC"
+end
 
 get '/' do
-	@barbers = Barber.order "created_at DESC"
 	erb :index
+end
+
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	Client.create :name => @username, :phone => @phone, :datestamp => @datetime, :barber => @barber, :color => @color
+
+	
+
+	erb "<h2>Спасибо, вы записались!</h2>"
+
 end
