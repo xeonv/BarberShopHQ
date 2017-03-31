23c44cf11903d5302e54f5e079e18befc148bca9
Valery <xeonv@mail.ru>
2017-03-31 23:45:49 +0400

    Create validation for cliens

--- app.rb
+++ app.rb
@@ -8,6 +8,10 @@ set :database, "sqlite3:barbershop.db"
 
 #создаем сущность (entity) ActiveRecord - пространство имен, а Base базовый класс
 class Client   < ActiveRecord::Base
+	validates :name, presence: true
+	validates :phone, presence: true
+	validates :datestamp, presence: true
+	validates :color, presence: true
 end
 
 class Barber < ActiveRecord::Base
